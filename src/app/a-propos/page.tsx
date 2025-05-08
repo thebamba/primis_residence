bb58b4270b68bb97de4ce482487eb43b8442e2f4
Cheikh Ahmadou Bamba Mboup <cheikh-ahmadou-bamba.mboup.1@ulaval.ca>
2025-05-08 17:05:43 -0400

    feat: Mise à jour de la partie à propos

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -1,14 +1,30 @@
+import { Linkedin } from "lucide-react";
+
 export default function About() {
-  return (
-    <section className="space-y-4">
-      <h1 className="text-3xl font-bold mb-2 text-rose-700">À propos</h1>
-      <p>
-        Bienvenue à la Résidence Soleil, un hébergement moderne situé au cœur de Dakar. Nous offrons confort, sécurité et hospitalité chaleureuse pour tous nos invités.
-      </p>
-      <h2 className="text-xl font-semibold mt-6">Votre hôte</h2>
-      <p>
-        La Résidence est gérée par M. Samba Ndiaye, passionné d’accueil et originaire du Sénégal. Toujours disponible, il veille à ce que chaque séjour soit unique et agréable.
-      </p>
-    </section>
-  );
+    return (
+        <section className="space-y-4">
+            <h1 className="text-3xl font-bold mb-2 text-rose-700">À propos</h1>
+            <p>
+                Bienvenue à la Résidence Primis, un hébergement moderne situé au cœur de Thiès. Nous offrons confort, sécurité et hospitalité chaleureuse pour tous nos invités.
+            </p>
+            <h2 className="text-xl font-semibold mt-6">À propos de la résidence</h2>
+            <p>
+                La résidence est exploitée par <strong className="inline-flex items-center gap-1">
+                PRIM’IS SARL
+                <a
+                    href="https://www.linkedin.com/in/prim-is-sarl-80498b214/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="LinkedIn de PRIM'IS"
+                >
+                    <Linkedin className="w-4 h-4 text-blue-600 hover:text-blue-800 transition" />
+                </a>
+            </strong>, une entreprise sénégalaise fondée en 2014 et spécialisée dans les services intellectuels, immobiliers et médicaux. Elle veille à offrir un accueil professionnel et chaleureux.
+            </p>
+            <h2 className="text-xl font-semibold mt-6">Votre hôte</h2>
+            <p>
+                Vous serez accueillie par <strong>Mme Binta Mbengue</strong> et <strong>M. Issa Mboup</strong>, professionnels de l’accueil dévoués, toujours à l’écoute pour rendre votre séjour agréable et sans souci.
+            </p>
+        </section>
+    );
 }
